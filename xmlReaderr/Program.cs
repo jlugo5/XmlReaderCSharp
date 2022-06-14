7db660295dc0134125d8cb535bd0432d469e0ee9
jlugo5 <45322802+jlugo5@users.noreply.github.com>
2022-06-14 10:59:27 -0400

    Add files via upload

---
+++ xmlReaderr/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml;
+using System.IO;
+
+//Console.WriteLine("Hello, World");
+
+namespace xmlreader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalc = 0;
+
+            XmlDocument xmlDoc = new XmlDocument();
+            string XMLpath = Directory.GetCurrentDirectory() + @"\info.xml";
+            xmlDoc.Load(XMLpath);
+
+            XmlNodeList Clist = xmlDoc.GetElementsByTagName("last-connected");
+            Console.WriteLine("Lsst connected VIN: " + Clist[0]?.Attributes?["vin"]?.InnerText.ToString() + " MFR: " + Clist[0]?.Attributes?["mfr"]?.InnerText.ToString());
+
+            Clist = xmlDoc.GetElementsByTagName("licenses");
+            Console.WriteLine("Total Licenses: " + Clist[0]?.Attributes?["available"]?.InnerText.ToString() + " Total Tuned Vehicles: " + Clist[0]?.Attributes?["tuned"]?.InnerText.ToString());
+
+            Clist = xmlDoc.GetElementsByTagName("license");
+            totalc = Clist.Count;
+            for (int i = 0; i < Clist.Count; i++)
+            {
+                
+                Console.WriteLine("Tuned VIN: " + Clist[i]?.Attributes?["vin"]?.InnerText.ToString() + " MFR: " + Clist[i]?.Attributes?["mfr"]?.InnerText.ToString());
+                //System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(totalc.ToString() + " version inXML File.");
+            Console.WriteLine(Clist);
+            Console.ReadLine();
+        }
+    }
+}
+
